927343eb3b1793ccd4e484dbe6ad3fa669bffb78
CDexNIHR <rob.waters@nihr.ac.uk>
2024-01-25 21:58:09 +0000

    event listener with enter

--- index.js
+++ index.js
@@ -5,17 +5,38 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
+
   let input = inputEl.value
   myLeads.push(input)
   inputEl.value = ""
   console.log(myLeads)
   renderInputs()
+
+})
+
+inputEl.addEventListener("keypress", function (e) {
+  if (e.key === 'Enter'){
+  let input = inputEl.value
+  myLeads.push(input)
+  inputEl.value = ""
+  console.log(myLeads)
+  renderInputs()
+  }
 })
 
 function renderInputs() {
   let listItems = ""
   for(let i = 0; i < myLeads.length; i++) {  
-    listItems += "<li><a href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"  
-  }   
+    
+    listItems += `
+      <li>
+        <a target='_blank' href='${myLeads[i]}'>
+          ${myLeads[i]}
+        </a>
+      </li>
+    `  
+  } 
+
   ulEl.innerHTML = listItems
+
 }
