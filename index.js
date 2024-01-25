e24f9a67c4e4b2de7f947f1151433b1f6bbb6b52
CDexNIHR <rob.waters@nihr.ac.uk>
2024-01-25 21:13:18 +0000

    lead tracker init

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
+  let input = inputEl.value
+  myLeads.push(input)
+  inputEl.value = ""
+  console.log(myLeads)
+  renderInputs()
+})
+
+function renderInputs() {
+  let listItems = ""
+  for(let i = 0; i < myLeads.length; i++) {  
+    listItems += "<li><a href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"  
+  }   
+  ulEl.innerHTML = listItems
+}
